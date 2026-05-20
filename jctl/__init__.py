468354d5d7b8030f4cc0966cf5518e2498c681bf
Avner Vidal <64892940+avnerv@users.noreply.github.com>
2026-05-20 11:25:58 +0300

    chore(main): release 0.3.1

--- jctl/__init__.py
+++ jctl/__init__.py
@@ -4,6 +4,6 @@ A command-line interface tool for managing Jenkins pipelines using
 Jenkins API token authentication.
 """
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __author__ = "Avner Vidal"
 __email__ = "avnervidal27@gmail.com"
